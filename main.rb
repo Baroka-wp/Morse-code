99e93110e8208af42b9750c11aee0df18fb0370c
baroka <birotori@gmail.com>
2022-07-11 20:22:57 +0100

    Fix linter reports

--- main.rb
+++ main.rb
@@ -1,35 +1,51 @@
-# Create a method to decode a Morse code character,
-# takes a string parameter, and return the corresponding character
+# Create a dictionary by placing a list of key/value pairs between braces,
+# with either a comma or the sequence => between the key and the value.
+
+MORSE_CODE = {
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z',
+  ' ' => ' ',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '-----' => '0'
+}.freeze
+
+# Create a method to decode a Morse code character, takes a string parameter,
+# and return the corresponding character in uppercase
+# (e.g. decode_char(".-") returns "A").
 def decode_char(char)
-  morse_code = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-': 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.-- ' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
-  }
-  morse_code[char]
+  MORSE_CODE[char].capitalize
 end
 
 # split sentence in worlds
@@ -37,7 +53,7 @@ def split_world(phrase)
   if phrase.is_a? String
     phrase.split('   ')
   else
-    put "Your inpu should be a string in double or single quote !"
+    put 'Your inpu should be a string in double or single quote !'
   end
 end
 
@@ -45,20 +61,23 @@ end
 # takes a string parameter, and return the string representation.
 
 def decode(phrase)
-  worlds = split_world (phrase)
+  worlds = split_world(phrase)
   phrase_decode = ''
   worlds.each do |world|
     world_decode = ''
-    world.split(' ').each do |morse|
-      world_decode += decode_char(morse)
+    world.chars.each do |morse|
+      dc = decode_char(morse)
+      world_decode += dc.to_s
     end
-    phrase_decode += world_decode + ' '
+    phrase_decode += "#{world_decode} "
   end
   phrase_decode
 end
 
 # Testing
-decode("-- -.--   -. .- -- .")
+decode('-- -.--   -. .- -- .')
+
 # => MY NAME
-phrase_2 = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-print decode(phrase_2) # => A BOX FULL OF RUBIES
+PHRASE = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'.freeze
+print decode(PHRASE) # => A BOX FULL OF RUBIES
+
